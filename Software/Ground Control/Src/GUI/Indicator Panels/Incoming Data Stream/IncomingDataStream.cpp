230911a409b99c852bfe130b11fdd3f0222965df
Jacob Chesley <jacob.a.chesley@gmail.com>
2015-10-31 15:31:11 -0400

    Incoming serial data stream display

---
+++ Software/Ground Control/Src/GUI/Indicator Panels/Incoming Data Stream/IncomingDataStream.cpp
@@ -0,0 +1,39 @@
+#include "IncomingDataStream.h"
+
+IncomingDataStream::IncomingDataStream(wxWindow * parent, wxString title) : wxPanel(parent){
+
+	allText = "";
+
+	this->SetBackgroundColour(wxColor(0, 0, 0));
+
+	titleText = new wxTextCtrl(this, -1);
+	titleText->SetBackgroundColour(wxColor(0, 0, 0));
+	titleText->SetForegroundColour(wxColor(255, 255, 255));
+	titleText->SetFont(wxFont(16, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+	titleText->SetValue(title);
+	titleText->SetEditable(false);
+
+	dataText = new wxTextCtrl(this, -1);
+	dataText->SetBackgroundColour(wxColor(0, 0, 0));
+	dataText->SetForegroundColour(wxColor(255, 255, 255));
+	dataText->SetFont(wxFont(12, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+	dataText->SetEditable(false);
+
+	layout = new wxBoxSizer(wxVERTICAL);
+	this->SetSizer(layout);
+
+	this->GetSizer()->Add(titleText, 0, wxEXPAND);
+	this->GetSizer()->Add(dataText, 1, wxEXPAND);
+
+	this->GetSizer()->Layout();
+}
+
+void IncomingDataStream::AppendText(wxString appendedText) {
+	allText += appendedText;
+	dataText->SetValue(allText);
+}
+
+void IncomingDataStream::ClearText() {
+	allText = "";
+	dataText->SetValue(allText);
+}
---
+++ Software/Ground Control/Src/GUI/Indicator Panels/Incoming Data Stream/IncomingDataStream.h
@@ -0,0 +1,31 @@
+#ifndef INCOMING_DATA_STREAM_H
+#define INCOMING_DATA_STREAM_H
+
+// for compilers that support precompilation, includes "wx/wx.h"
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers explicitly
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+
+class IncomingDataStream : public wxPanel {
+
+public:
+	IncomingDataStream(wxWindow * parent, wxString title);
+	void AppendText(wxString appendedText);
+	void ClearText();
+
+private:
+
+	wxBoxSizer * layout;
+	wxTextCtrl * titleText;
+	wxTextCtrl * dataText;
+	
+	wxString allText;
+
+
+};
+
+#endif
