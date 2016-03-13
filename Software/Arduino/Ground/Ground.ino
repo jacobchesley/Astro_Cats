9edf619c377e37f7112d5f1778ecfaf36263d544
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-03-13 16:53:51 -0400

    Update Ground and Tracking software

--- Software/Arduino/Ground/Ground.ino
+++ Software/Arduino/Ground/Ground.ino
@@ -1,31 +1,149 @@
+#include <ArduinoJson.h>
 #include <RadioAPI.h>
+#include <RadioINIT.h>
 
 #define SHUTDOWN_PIN 22
 #define BINARY_PIN 23
 
 RadioAPI * radio;
+RadioINIT * radioAddresses;
+
+int lastTrackingSignal;
+int lastPILSignal;
+String incomingMessage;
+byte frameID;
+
 void setup() {
 
   Serial.begin(115200);
   Serial1.begin(115200);
   delay(100);
-  
+
+  while(Serial.available()){
+    Serial.read();
+  }
+  while(Serial1.available()){
+    Serial1.read();
+  }
   radio = new RadioAPI(&Serial1, SHUTDOWN_PIN, BINARY_PIN);
+  radioAddresses = new RadioINIT();
+  
   radio->ClearRXArray();
+  incomingMessage = "";
+  frameID = 0x01;
+  Serial.println("Ground Ready");
 }
 
 void loop() {
-    
+
  radio->GetPackets();
  int numRXPackets = radio->GetRXSize();
  RadioPacketRX * rxPackets = radio->GetRXArray();
  
  for(int i = 0; i < numRXPackets; i++){
   RadioPacketRX tempRX = rxPackets[i];
+
   for(int byt = 0; byt < tempRX.dataLength; byt++){
     Serial.print((char)tempRX.data[byt]);
   }
+  
+  if(tempRX.sourceAddress == radioAddresses->TRACKING_ADDRESS_INT){
+    SendSignalStrength("Tracking", tempRX.signalStrength);
+  }
+  if(tempRX.sourceAddress == radioAddresses->PIL_ADDRESS_INT){
+    SendSignalStrength("PIL", tempRX.signalStrength);
+  }
  }
  radio->ClearRXArray();
- 
+
+ int previousMessageLength = incomingMessage.length();
+ CheckIncomingMessages();
+ int newMessageLength = incomingMessage.length();
+ if(newMessageLength == previousMessageLength && newMessageLength != 0){
+  SendMessage();
+  incomingMessage = "";
+ }
 }
+
+void SendSignalStrength(String source, int signalStrength){
+
+   // Create JSON Buffer
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& trackingDataObject = jsonBuffer.createObject();
+  
+  // Set GPS Data
+  trackingDataObject["Source"] = "Ground";
+  trackingDataObject["MessageType"] = "SignalStrength";
+  trackingDataObject["SignalSource"] = source;
+  trackingDataObject["SignalStrength"] = signalStrength;
+  //trackingDataObject.printTo(Serial);
+  //Serial.print('\r');
+  //Serial.print('\n');
+  
+  // Initialize char array
+  int len = trackingDataObject.measureLength();
+  char * jsonCharBuffer = new char[len + 2];
+  for(int i = 0; i < len+2; i++){
+    jsonCharBuffer[i] = ' ';
+  }
+
+  trackingDataObject.printTo(jsonCharBuffer, len+1);
+  jsonCharBuffer[len] = '\r';
+  jsonCharBuffer[len+1] = '\n';
+
+  for(int i = 0; i < len+2; i++){
+    Serial.print(jsonCharBuffer[i]);
+  }
+  delete[]jsonCharBuffer;
+  jsonCharBuffer = NULL;
+  
+}
+
+void CheckIncomingMessages(){
+
+  while(Serial.available()){
+    incomingMessage += (char) Serial.read();
+  }
+}
+
+int GetMessageDestination(){
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& command = jsonBuffer.parseObject(incomingMessage);
+
+  const char * dest = command["Destination"];
+  String destination(dest);
+  if(destination == "PIL"){
+    return radioAddresses->PIL_ADDRESS_INT;
+  }
+  if(destination == "Rocket"){
+    return radioAddresses->TRACKING_ADDRESS_INT;
+  }
+  return -1;
+}
+
+void SendMessage(){
+
+  Serial.println(incomingMessage);
+  int destination = GetMessageDestination();
+
+  if(destination < 0){ return; }
+
+  int len = incomingMessage.length();
+  char * jsonCharBuffer = new char[len + 2];
+  for(int i = 0; i < len+2; i++){
+    jsonCharBuffer[i] = ' ';
+  }
+
+  for(int i = 0; i < len; i++){
+    jsonCharBuffer[i] = incomingMessage[i];
+  }
+  jsonCharBuffer[len] = '\r';
+  jsonCharBuffer[len+1] = '\n';
+  byte * jsonByteBuffer = (byte*)jsonCharBuffer;
+  
+  RadioPacketTXR transmitPacket = radio->BuildTXRPacket(jsonByteBuffer, len+2, destination, frameID);
+  radio->SendTXRPacket(transmitPacket);
+}
+
+
--- Software/Arduino/Tracking/Tracking.ino
+++ Software/Arduino/Tracking/Tracking.ino
@@ -9,8 +9,16 @@ RadioAPI * radio;
 Venus638 * gps;
 GPSData gpsData;
 
+String incomingMessage;
+String lastCommand;
+String lastCommandValue;
+
+long microsecondCount;
+long microsecondUpdate;
+
 void setup() {
 
+  
   // Output serial
   Serial.begin(115200);
 
@@ -37,30 +45,129 @@ void setup() {
   radio = new RadioAPI(&Serial1, SHUTDOWN_PIN, BINARY_PIN);
   radio->ClearRXArray();
   radio->ClearTXSArray();
+
+  incomingMessage = "";
+  lastCommand = "";
+  lastCommandValue = "";
+  microsecondCount = 0;
+  microsecondUpdate = 1000000;
+
 }
 
 void loop() {
 
   // Get latest GPS Data and fill in gps data struct
-  gps->Update();
-  gps->FillInGPSData(&gpsData);
+  if(microsecondCount > microsecondUpdate){
+    gps->Update();
+    gps->FillInGPSData(&gpsData);
   
-  // Send json string over radio
-  SendJSONData();
+    // Send json string over radio
+    SendJSONData();
+    microsecondCount = 0;
+   }
 
-  // Delay 0.5 seconds if satellites are locked in, otherwise delay 2 seconds
-  if(gpsData.NumSatellites > 0){ delay(500); }
-  else{ delay(2000); }
+  radio->GetPackets();
+  int numRXPackets = radio->GetRXSize();
+  RadioPacketRX * rxPackets = radio->GetRXArray();
+ 
+  for(int i = 0; i < numRXPackets; i++){
+    RadioPacketRX tempRX = rxPackets[i];
+    Serial.println("Got Something");
+    for(int byt = 0; byt < tempRX.dataLength; byt++){
+      incomingMessage += (char)tempRX.data[byt];
+    }
+    ParseMessage();
+    ExecuteLastCommand();
+  }
 
   radio->ClearRXArray();
   radio->ClearTXSArray();
+
+  delayMicroseconds(100);
+  microsecondCount += 100;
+}
+
+void ParseMessage(){
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& command = jsonBuffer.parseObject(incomingMessage);
+
+  const char * dest = command["Destination"];
+  const char * type = command["MessageType"];
+  
+  String destination(dest);
+  String messageType(type);
+  if(destination == "Rocket" && messageType == "Command"){
+    const char * cmd = command["Command"];
+    const char * val = command["Value"];
+
+    String command(cmd);
+    String value(val);
+
+    lastCommand = command;
+    lastCommandValue = value;
+  }
+  incomingMessage = "";
+}
+
+void ExecuteLastCommand(){
+  if(lastCommand == "RadioPower"){
+    int powerLevel = lastCommandValue.toInt();
+    if(powerLevel < 0 || powerLevel > 4){ return; }
+
+      byte * radioPowerLevel = new byte[1];
+      radioPowerLevel[0] = powerLevel & 0xFF;
+      RadioPacketAT powerLevelAPIPacket = radio->BuildATCommandPacket('P', 'L', radioPowerLevel, 1);
+      radio->SendATCommandPacket(powerLevelAPIPacket);
+      SendMessageConfirmation();
+  }
+
+  if(lastCommand == "UpdateInterval"){
+    char * charVal = new char[lastCommandValue.length() + 1];
+    lastCommandValue.toCharArray(charVal, lastCommandValue.length() + 1);
+    microsecondUpdate = atol(charVal);
+    SendMessageConfirmation();
+  }
+}
+
+void SendMessageConfirmation(){
+
+  // Create JSON Buffer
+  StaticJsonBuffer<150> jsonBuffer;
+  JsonObject& confirmationDataObject = jsonBuffer.createObject();
+  
+  // Set Confirmation Data
+  confirmationDataObject["Source"] = "Rocket";
+  confirmationDataObject["MessageType"] = "CommandResponse";
+  confirmationDataObject["Command"] = lastCommand;
+  confirmationDataObject["Value"] = lastCommandValue;
+
+  // Initialize char and byte array
+  int len = confirmationDataObject.measureLength();
+  char * jsonCharBuffer = new char[len + 2];
+  for(int i = 0; i < len+2; i++){
+    jsonCharBuffer[i] = ' ';
+  }
+  confirmationDataObject.printTo(jsonCharBuffer, len+1);
+  jsonCharBuffer[len] = '\r';
+  jsonCharBuffer[len+1] = '\n';
+  byte * jsonByteBuffer = (byte*)jsonCharBuffer;
+  
+  RadioPacketTXR transmitPacket = radio->BuildTXRPacket(jsonByteBuffer, len+2, 4794, 0x01);
+  radio->SendTXRPacket(transmitPacket);
+  
+  delete[] transmitPacket.data;
+  transmitPacket.data = NULL;
+
+  delete[]jsonCharBuffer;
+  jsonCharBuffer = NULL;
 }
 
 
 void SendJSONData(){
   
   // Create JSON Buffer
-  StaticJsonBuffer<500> jsonBuffer;
+  StaticJsonBuffer<400> jsonBuffer;
   JsonObject& trackingDataObject = jsonBuffer.createObject();
   
   // Set GPS Data
@@ -81,16 +188,17 @@ void SendJSONData(){
   
   // Initialize char and byte array
   int len = trackingDataObject.measureLength();
-  char * jsonCharBuffer = new char[len + 4];
-  for(int i = 0; i < len+4; i++){
+  char * jsonCharBuffer = new char[len + 2];
+  for(int i = 0; i < len+2; i++){
     jsonCharBuffer[i] = ' ';
   }
   trackingDataObject.printTo(jsonCharBuffer, len+1);
-  jsonCharBuffer[len+2] = '\r';
-  jsonCharBuffer[len+3] = '\n';
+ 
+  jsonCharBuffer[len] = '\r';
+  jsonCharBuffer[len+1] = '\n';
   byte * jsonByteBuffer = (byte*)jsonCharBuffer;
   
-  RadioPacketTXR transmitPacket = radio->BuildTXRPacket(jsonByteBuffer, len+4, 4794, 0x01);
+  RadioPacketTXR transmitPacket = radio->BuildTXRPacket(jsonByteBuffer, len+2, 4794, 0x01);
   radio->SendTXRPacket(transmitPacket);
   
   delete[] transmitPacket.data;
