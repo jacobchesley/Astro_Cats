fca14fc063782a6a3b2c5c55368ceadf2f80c618
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-10 15:36:16 -0400

    Add PIL arduino code

--- Software/Arduino/PIL/PIL.ino
+++ Software/Arduino/PIL/PIL.ino
@@ -1,42 +1,35 @@
+#include <SPI.h>
 #include <SD.h>
 #include<Wire.h>
+#include <Servo.h>
 
-#include <Accelerometer.h>
+#include <adxl345.h>
 #include <ArduinoJson.h>
+#include <Camera.h>
 #include <dht.h>
 #include <ML8511.h>
 #include <PIL.h>
+#include <RadioAPI.h>
+#include <RadioINIT.h>
 #include <SFE_BMP180.h>
+#include <SP215.h>
 #include <Venus638.h>
-
-// Sensor Pins
-int pin_TempHumid = 22;
-int CS_PIN = 53;
 
 PIL *pil;
 
 void setup() {
-  Serial.begin(115200);
-  Serial2.begin(9600);
 
-  // Setup SD Card
-  pinMode(CS_PIN, OUTPUT);
-  
-  // see if the card is present
-  if (!SD.begin(CS_PIN)){
-    Serial.println("SD Card failed to initialize");
-  }
-  else{
-    Serial.println("SD Card Initialized!");
-  }
- 
-  pil = new PIL(pin_TempHumid);
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial2.begin(9600);
+  Serial3.begin(115200);
+  delay(50);
+
+  pil = new PIL();
+  Serial.println("Made it to end of setup");
 }
 
 void loop() {
 
-  pil->UpdateAll();
-  pil->SendJsonPilDataSerial(&Serial);
-  Serial.println("");
-  delay(300);
+  pil->Loop();
 }
