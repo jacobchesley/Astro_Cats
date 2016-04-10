55eb714d86d87eedf27cf57740e2c950e8bd3379
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-10 15:32:31 -0400

    Add PIL library

--- Software/Arduino/Libraries/PIL/PIL.cpp
+++ Software/Arduino/Libraries/PIL/PIL.cpp
@@ -1,29 +1,81 @@
 #include "PIL.h"
 
-PIL::PIL(int tempHumidPin){
+PIL::PIL(){
 
-	// Create new DHT sensor
-	tempHumidity = new dht(tempHumidPin);
+	nextFrameID = 0x00;
 
-	// Create new UV Sensor. analog pin 0 is output, 1 is reference
-	uv = new ML8511(0, 1);
+	// Radio Serial
+	Serial1.begin(115200);
 
-	// Create new accelerometer and configure.  SDO is grounded
-	accelerometer = new Accelerometer(true);
-	accelerometer->Configure();
+	// GPS Serial
+	Serial2.begin(9600);
 
-	// Create new BMP180 sensor
-	pressure = new SFE_BMP180();
-	// Initialize the pressure sensor, and check for failure
-	if(!pressure->begin()){
+	delay(50);
 
+	while(Serial1.available()){
+		Serial1.read();
 	}
 
-	gps = new Venus638(&Serial2);
-	gps->SetBaudRate(Venus638::baud_115200);
+	microsecondCount = 0;
+	microsecondUpdate = 500000;
+
+	// see if the card is present
+	if(!SD.begin(53)){}
+	else{}
+
+	// Create new accelerometer and configure.  SDO is grounded
+	if(!accelerometer.begin()){  	}
+  	else{
+  		accelerometer.setRange(ADXL345_RANGE_16_G);
+  	}
+
+	// Initialize the pressure sensor, and check for failure
+	if(!pressure.begin()){}
+
+	radio.ClearRXArray();
+	radio.ClearTXSArray();
+
+	gps.SetBaudRate(Venus638::baud_115200);
 	Serial2.begin(115200);
 
-	gps->SetUpdateRate(Venus638::update_20);
+	delay(50);
+	gps.SetUpdateRate(Venus638::update_20);
+
+	// Camera Serial
+	bool camIsHighspeed = false;;
+	Serial3.begin(256000);
+
+	delay(50);
+	
+	// Try resetting camera at higher baud (in case power was left on from last baud set)
+	if(camera.Reset()){}
+
+	// Camera is not at high baud, set it there.
+	else{
+		Serial3.begin(115200);
+		delay(50);
+
+		if(camera.SetBaud(camera.baud_256000)){
+			Serial3.begin(256000);
+			delay(50);
+		}
+		else{
+		}
+
+	}
+	
+	if(!camera.SetSize(camera.size_640_480)){
+	}
+
+	//delay(2000);
+	//this->TakeAndStorePhoto();
+	Serial.println("done init");
+/*
+	for(int i = 0; i < 10; i++){
+		delay(1000);
+	}
+	this->OpenParachute();
+	*/
 }
 
 void PIL::UpdateAll(){
@@ -32,40 +84,81 @@ void PIL::UpdateAll(){
 	this->UpdatePressure();
 	this->UpdateAcceleration();
 	this->UpdateUV();
+	this->UpdateSolar();
 	this->UpdateGPS();
+}
+
+void PIL::Loop(){
+
+	// Get latest PIL Data
+	if(microsecondCount > microsecondUpdate){
+
+		Serial.println("Updating and sending...");
+		this->UpdateAll();
+		// Send tracking string over radio, time since last transfer is now 0 microseconds
+
+		SendPilData();
+		microsecondCount = 0;
+		Serial.println("Done updating and sending");
+	}
+
+	radio.GetPackets();
+
+	int numRXPackets = radio.GetRXSize();
+
+	RadioPacketRX * rxPackets = radio.GetRXArray();
+ 	
+ 	// Go through all incoming radio packets
+	for(int i = 0; i < numRXPackets; i++){
+
+		Serial.println("Radio packet recieved");
+		// Get each radio packet
+		RadioPacketRX tempRX = rxPackets[i];
+
+		String incomingMessage = "";
+		// Get message from packet data
+		for(int byt = 0; byt < tempRX.dataLength; byt++){
+			incomingMessage += (char)tempRX.data[byt];
+		}
+
+		// Parse and execute the incoming command
+		this->ParseMessage(incomingMessage);
+	}
+
+	radio.ClearRXArray();
+	radio.ClearTXSArray();
+
+	delayMicroseconds(100);
+	microsecondCount += 100;
 }
 
 // Updates the tempurature and humidity inside class variable pilData
 void PIL::UpdateTempHumidity(){
 
 	// Read the temperature and humidity data
-	int result = tempHumidity->read22();
+	int result = tempHumidity.read22();
 
 	switch (result){
 
 		// Uodate the pilData values if result is okay
 		case DHTLIB_OK:  
-			pilData.Temperature = tempHumidity->temperature;
-			pilData.Humidity = tempHumidity->humidity; 
-			pilData.TempHumidityError = "";
+			pilData.Temperature = tempHumidity.temperature;
+			pilData.Humidity = tempHumidity.humidity; 
 			break;
 
 		case DHTLIB_ERROR_CHECKSUM: 
 			pilData.Temperature = -9999;
 			pilData.Humidity = -9999;
-			pilData.TempHumidityError = "Checksum error";
 			break;
 
 		case DHTLIB_ERROR_TIMEOUT: 
 			pilData.Temperature = -9999;
 			pilData.Humidity = -9999;
-			pilData.TempHumidityError = "Time out error";
 			break;
 
 		default: 
 			pilData.Temperature = -9999;
 			pilData.Humidity = -9999;
-			pilData.TempHumidityError = "Unknown error";
 			break;
 	}	
 }
@@ -73,7 +166,7 @@ void PIL::UpdateTempHumidity(){
 void PIL::UpdatePressure(){
 	
 	// Start bmp180 tempurature measurement
-	int delayTemp = pressure->startTemperature();
+	int delayTemp = pressure.startTemperature();
 
 	// If the requested delay is < 0,  there is an error.
 	if(delayTemp <= 0){
@@ -86,7 +179,7 @@ void PIL::UpdatePressure(){
 	delay(delayTemp);
 	
 	// Read the tempurature from BMP180 and check for error
-	if(!pressure->getTemperature(pilData.PressureTemp)){
+	if(!pressure.getTemperature(pilData.PressureTemp)){
 		pilData.Pressure = -9999;
 		pilData.PressureTemp = -9999;
 		return;
@@ -94,7 +187,7 @@ void PIL::UpdatePressure(){
 
 	// Start BMP180 pressure measurement
 	// oversampling value provided, 0 (lowest resolution) - 3 (highest resolution)
-	int delayPressure = pressure->startPressure(3);
+	int delayPressure = pressure.startPressure(3);
 
 	// If the requested delay is < 0,  there is an error.
 	if(delayPressure <= 0){
@@ -106,7 +199,7 @@ void PIL::UpdatePressure(){
 	delay(delayPressure);
 
 	// Read the pressure from BMP180 and check for error
-	if(!pressure->getPressure(pilData.Pressure, pilData.PressureTemp)){
+	if(!pressure.getPressure(pilData.Pressure, pilData.PressureTemp)){
 		pilData.Pressure = -9999;
 		pilData.PressureTemp = -9999;
 		return;
@@ -114,35 +207,74 @@ void PIL::UpdatePressure(){
 }
 
 void PIL::UpdateAcceleration(){
-	pilData.AccelerationX = accelerometer->GetAccelerationX();
-	pilData.AccelerationY = accelerometer->GetAccelerationY();
-	pilData.AccelerationZ = accelerometer->GetAccelerationZ();
+
+	sensors_event_t event; 
+  	accelerometer.getEvent(&event);
+	pilData.AccelerationX = event.acceleration.x;
+	pilData.AccelerationY = event.acceleration.y;
+	pilData.AccelerationZ = event.acceleration.z;
 }
 
 void PIL::UpdateUV(){
-	pilData.UV = uv->GetUV();
+	pilData.UV = uv.GetUV();
+}
+
+void PIL::UpdateSolar(){
+	pilData.Solar = solar.GetSolar();
 }
 
 void PIL::UpdateGPS(){
-	gps->Update();
-	gps->FillInGPSData(&pilData.gpsData);
+	gps.Update();
+	gps.FillInGPSData(&pilData.gpsData);
 }
 
+void PIL::TakeAndStorePhoto(){
+	
+	SD.remove("Photo1.jpg");
+	File photoFile = SD.open("Photo1.jpg", FILE_WRITE);
+
+	if(!photoFile){
+		Serial.println("File bad");
+		return;
+	}
+
+	if(camera.Capture()){
+		Serial.println("Camera Capture Good!");
+	}
+	else{
+		Serial.println("Captute bad?");
+	}
+	unsigned long StartTime = millis();
+
+	while(camera.ContinueRead()){
+		camera.SendNextPartOfFile(&photoFile);
+	}
+	unsigned long CurrentTime = millis();
+	unsigned long ElapsedTime = CurrentTime - StartTime;
+	Serial.println("Done!");
+	Serial.println(ElapsedTime/1000.0f);
+	
+	photoFile.close();
+}
 
 PILData PIL::GetPilData(){
 	return pilData;
 }
 
-void PIL::SendJsonPilDataSerial(HardwareSerial * serial){
+void PIL::SendPilData(){
 
-	StaticJsonBuffer<200> jsonBuffer;
+	StaticJsonBuffer<400> jsonBuffer;
 	JsonObject& pilDataObject = jsonBuffer.createObject();
+
+	pilDataObject["Source"] = "PIL";
+	pilDataObject["MessageType"] = "Data";
 
 	// Set atmospheric data
 	pilDataObject["Temp"] = pilData.Temperature;
 	pilDataObject["Humidity"] = pilData.Humidity;
 	pilDataObject["Pressure"] = pilData.Pressure;
 	pilDataObject["UV"] = pilData.UV;
+	pilDataObject["Solar"] = pilData.Solar;
 
 	// Set acceleration data
 	pilDataObject["AccelerationX"] = pilData.AccelerationX;
@@ -151,25 +283,159 @@ void PIL::SendJsonPilDataSerial(HardwareSerial * serial){
 
 	// Set GPS Data
 	pilDataObject["Time"] = pilData.gpsData.Time;
-	pilDataObject["Lat"] = pilData.gpsData.Latitude;
-	pilDataObject["NS"] = pilData.gpsData.NorthSouth;
-	pilDataObject["Lon"] = pilData.gpsData.Longitude;
-	pilDataObject["EW"] = pilData.gpsData.EastWest;
+	pilDataObject["Lat"].set(gps.DMSToDecimal(pilData.gpsData.Latitude), 5);
+	pilDataObject["NS"] = (String)pilData.gpsData.NorthSouth;
+	pilDataObject["Lon"].set(gps.DMSToDecimal(pilData.gpsData.Longitude), 5);
+	pilDataObject["EW"] = (String)pilData.gpsData.EastWest;
 	pilDataObject["Altitude"] = pilData.gpsData.Altitude;
 	pilDataObject["Quality"] = pilData.gpsData.Quality;
-	pilDataObject["NumSat"] = pilData.gpsData.NumSatellites;
+	pilDataObject["PDOP"] = pilData.gpsData.PDOP;
 	pilDataObject["HDOP"] = pilData.gpsData.HDOP;
+	pilDataObject["VDOP"] = pilData.gpsData.VDOP;
+	pilDataObject["NumSat"] = pilData.gpsData.NumSatellites;
+	pilDataObject["SatList"] = pilData.gpsData.SatelliteList;
 
-	pilDataObject.printTo(Serial);
+	// Initialize char and byte array
+	int len = pilDataObject.measureLength();
 
-	sdFile = SD.open("Test.astrocat", FILE_WRITE);
-	if(sdFile){
-		char sdOutput[200];
-		pilDataObject.printTo(sdOutput, sizeof(sdOutput));
-		sdFile.println(sdOutput);
+	//char * jsonCharBuffer = new char[len + 2];
+	char jsonCharBuffer[400];
+	for(int i = 0; i < len + 2; i++){
+		jsonCharBuffer[i] = ' ';
 	}
-	else{
-		Serial.println("sd failure");
+
+	pilDataObject.printTo(jsonCharBuffer, len + 1);
+
+	jsonCharBuffer[len] = '\r';
+	jsonCharBuffer[len+1] = '\n';
+	byte * jsonByteBuffer = (byte*)jsonCharBuffer;
+
+	byte id = this->GetNextFrameID();
+	RadioPacketTXR transmitPacket = radio.BuildTXRPacket(jsonByteBuffer, len + 2, radioAddresses.GROUND_ADDRESS_INT, id);
+	radio.SendTXRPacket(transmitPacket);
+
+	//pilDataObject.printTo(Serial);
+}
+
+void PIL::ParseMessage(String message){
+
+	// Create JSON from incoming message
+	StaticJsonBuffer<200> jsonBuffer;
+	JsonObject& command = jsonBuffer.parseObject(message);
+
+	// Get Destination and Message type from JSON
+	const char * dest = command["Destination"];
+	const char * type = command["MessageType"];
+  
+	String destination(dest);
+	String messageType(type);
+
+	// Verify the destination is for the rocket and the message type is a command
+	if(destination == "PIL" && messageType == "Command"){
+		const char * cmd = command["Command"];
+		const char * val = command["Value"];
+
+		// Set the last command and last command value that will be executed in ExecuteLastCommand
+		String command(cmd);
+		String value(val);
+
+		this->ExecuteCommand(command, value);
 	}
-	sdFile.close();
+}
+
+void PIL::ExecuteCommand(String commandName, String commandValue){
+
+	if(commandName == "CaptureImage"){
+
+	}
+
+	if(commandName == "ListFiles"){
+
+	}
+
+	if(commandName == "LockParachute"){
+		this->CloseParachute();
+		this->SendMessageConfirmation(commandName, "SUCCESS");
+	}
+
+	if(commandName == "ReleaseParachute"){
+		this->OpenParachute();
+		this->SendMessageConfirmation(commandName, "SUCCESS");
+	}
+
+	// PIL Data Update and Send Interval in Microseconds
+	if(commandName == "UpdateInterval"){
+
+		// Get char array from lastCommandValue string
+		char * charVal = new char[commandValue.length() + 1];
+		commandValue.toCharArray(charVal, commandValue.length() + 1);
+
+		// Set microsecondUpdate from char array
+		microsecondUpdate = atol(charVal);
+
+		// Send confirmation message to ground
+		this->SendMessageConfirmation(commandName, commandValue);
+
+		delete[] charVal;
+	}
+}
+
+void PIL::SendMessageConfirmation(String commandName, String response){
+	// Create JSON Buffer
+	StaticJsonBuffer<200> jsonBuffer;
+	JsonObject& confirmationDataObject = jsonBuffer.createObject();
+
+	// Set Confirmation Data
+	confirmationDataObject["Source"] = "PIL";
+	confirmationDataObject["MessageType"] = "CommandResponse";
+	confirmationDataObject["Command"] = commandName;
+	confirmationDataObject["Value"] = response;
+
+	// Initialize char array
+	int len = confirmationDataObject.measureLength();
+	char  jsonCharBuffer[200];
+	for(int i = 0; i < len + 2; i++){
+		jsonCharBuffer[i] = ' ';
+	}
+
+	// Print JSON Buffer to char array
+	confirmationDataObject.printTo(jsonCharBuffer, len + 1);
+
+	// New lines for ease of reading data on ground control
+	jsonCharBuffer[len] = '\r';
+	jsonCharBuffer[len+1] = '\n';
+
+	// Set byte buffer to point to char array
+	byte * jsonByteBuffer = (byte*)jsonCharBuffer;
+  	
+  	byte id = this->GetNextFrameID();
+  	// Build and send radio transmit packet to ground control
+	RadioPacketTXR transmitPacket = radio.BuildTXRPacket(jsonByteBuffer, len + 2, radioAddresses.GROUND_ADDRESS_INT, id);
+	radio.SendTXRPacket(transmitPacket);
+
+  
+}
+
+void PIL::OpenParachute(){
+	motor.attach(8);
+	motor.write(180);
+	delay(2000);
+	motor.detach();
+}
+
+void PIL::CloseParachute(){
+	motor.attach(8);
+	motor.write(90);
+	delay(2000);
+	motor.detach();
+}
+
+byte PIL::GetNextFrameID(){
+	nextFrameID += 1;
+	nextFrameID = nextFrameID % 255;
+	if(nextFrameID == 0){
+		nextFrameID += 1;
+	}
+	return nextFrameID & 0xFF;
+
 }
--- Software/Arduino/Libraries/PIL/PIL.h
+++ Software/Arduino/Libraries/PIL/PIL.h
@@ -1,19 +1,21 @@
 #include <arduino.h>
-#include <SD.h>
 
 #include "../DHTLib/dht.h"
 #include "../BMP180/SFE_BMP180.h"
-#include "../Accelerometer/Accelerometer.h"
+#include "../ADXL345/ADXL345.h"
 #include "../UV/ML8511.h"
+#include "../Pyranometer/SP215.h"
 #include "../GPS/Venus638.h"
 #include "../ArduinoJson/include/ArduinoJson.h"
+#include "../RadioAPI/RadioAPI.h"
+#include "../RadioINIT/RadioINIT.h"
+#include "../Camera/Camera.h"
+#include "../Servo/src/Servo.h"
 
 struct PILData{
 
 	double Temperature;
 	double Humidity;
-	String TempHumidityError;
-
 
 	double UV;
 	double Solar;
@@ -30,7 +32,9 @@ struct PILData{
 
 class PIL{
 public:
-	PIL(int tempHumidPin);
+	PIL();
+
+	void Loop();
 
 	void CreateJSONFromPILData(PILData pilData);
 	void CreatePILDataFromJson();
@@ -39,20 +43,44 @@ public:
 	void UpdatePressure();
 	void UpdateAcceleration();
 	void UpdateUV();
+	void UpdateSolar();
 	void UpdateGPS();
+	void TakeAndStorePhoto();
 
 	void UpdateAll();
 
 	PILData GetPilData();
-	void SendJsonPilDataSerial(HardwareSerial * serial);
+	void SendPilData();
 
 private:
-	dht * tempHumidity;
-	SFE_BMP180 * pressure;
-	Accelerometer * accelerometer;
-	ML8511 * uv;
-	Venus638 * gps;
+
+	void ParseMessage(String message);
+	void ExecuteCommand(String commandName, String commandValue);
+	void SendMessageConfirmation(String commandName, String response);
+	byte GetNextFrameID();
+
+	void OpenParachute();
+	void CloseParachute();
+
+	dht tempHumidity = dht(26);
+	SFE_BMP180 pressure = SFE_BMP180();
+	Adafruit_ADXL345_Unified accelerometer = Adafruit_ADXL345_Unified(12345);
+	ML8511  uv = ML8511(0, 1);
+	SP215 solar = SP215(2);
+	Venus638 gps = Venus638(&Serial2);
 
 	PILData pilData;
+
+	RadioAPI radio = RadioAPI(&Serial1, 22);
+	RadioINIT radioAddresses = RadioINIT();
+	int nextFrameID;
+
+	long microsecondCount;
+	long microsecondUpdate;
+
 	File sdFile;
+
+	Camera camera = Camera(&Serial3);
+
+	Servo motor;
 };
